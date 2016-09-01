b97b77f561103c787dff829fad7c5512e88755c9
WeRockStar <vr_freestyle@outlook.com>
2016-09-01 21:29:03 +0700

    Added firebase lib

--- app/src/main/java/com/werockstar/firebaseanalytics/di/AndroidModule.java
+++ app/src/main/java/com/werockstar/firebaseanalytics/di/AndroidModule.java
@@ -1,4 +1,17 @@
 package com.werockstar.firebaseanalytics.di;
 
+import android.app.Activity;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
 public class AndroidModule {
+
+    private Activity activity;
+
+    public AndroidModule(Activity activity) {
+        this.activity = activity;
+    }
+
 }
