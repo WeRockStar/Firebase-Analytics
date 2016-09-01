c12f5ddb22d3f54fbdbbaaa437f5aa8ebf7f1822
WeRockStar <vr_freestyle@outlook.com>
2016-09-01 22:45:22 +0700

    Try to send your event

--- app/src/main/java/com/werockstar/firebaseanalytics/view/MainActivity.java
+++ app/src/main/java/com/werockstar/firebaseanalytics/view/MainActivity.java
@@ -3,6 +3,7 @@ package com.werockstar.firebaseanalytics.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.werockstar.firebaseanalytics.MainApplication;
@@ -10,10 +11,17 @@ import com.werockstar.firebaseanalytics.R;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     @Inject
     FirebaseAnalytics analytics;
+
+    @BindView(R.id.btnSendEvent)
+    Button btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +30,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+    }
 
-        if(analytics != null)
-            Log.d("Dagger", "Injected");
-        else
-            Log.d("Dagger", "Not Inject");
+    @OnClick(R.id.btnSendEvent)
+    void onSendEvent() {
+        analytics.logEvent("EVENT_TEST", null);
     }
 }
